48c26248f8b9e622b222c64cc672afb006332c77
Lewis Ledesma <lewis.ledesma@gmail.com>
2021-05-14 11:23:34 -0400

    added a feature function

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -3,5 +3,10 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Mami!");
+        feature();
+    }
+
+    public static void feature(){
+        System.out.println("This is a feature");
     }
 }
